bf86b7714658cc022661f5c61e97a68ec18bbf38
Kevin Willian <kevinwillian084@gmail.com>
2025-02-13 23:04:44 -0300

    feat: add orders page to router file

---
+++ src/pages/app/dashboard.tsx
@@ -0,0 +1,10 @@
+import { Helmet } from 'react-helmet-async'
+
+export function Dashbord() {
+  return (
+    <div>
+      <Helmet title="Dashboard" />
+      Dashboard
+    </div>
+  )
+}
--- src/routes.tsx
+++ src/routes.tsx
@@ -7,6 +7,7 @@ import {
 import { AppLayout } from './pages/_layouts/app'
 import { AuthLayout } from './pages/_layouts/auth'
 import { Dashbord } from './pages/app/dashboard'
+import { Orders } from './pages/app/orders/orders'
 import { SignIn } from './pages/auth/sign-in'
 import { SignUp } from './pages/auth/sign-up'
 
@@ -15,6 +16,7 @@ export const router = createBrowserRouter(
     <Route>
       <Route path="/" element={<AppLayout />}>
         <Route path="/" element={<Dashbord />} />
+        <Route path="/orders" element={<Orders />} />
       </Route>
 
       <Route path="/" element={<AuthLayout />}>
